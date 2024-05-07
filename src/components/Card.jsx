0a8243d50048ae6abfe3fcf3ec0d4d2a7f3d7535
dharamthokpranav <dharamthokpranav@gmail.com>
2024-05-07 03:02:22 -0700

    New project added

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = ({ children, bg = "bg-gray-100" }) => {
+  return <div className={`${bg} p-6 rounded-lg shodow-md`}>{children}</div>;
+};
+
+export default Card;
