70e30ed4827982ae5c92513b40df848c0c195eaf
dharamthokpranav <dharamthokpranav@gmail.com>
2024-05-25 01:13:28 -0700

    Final changes pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import jobPage from "./pages/jobPage";
+import SingleJobPage, { jobLoader } from "./pages/SingleJobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 import {
   RouterProvider,
   createBrowserRouter,
@@ -11,19 +13,67 @@ import {
   Route,
 } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<jobPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
-
 const App = () => {
+  //add new job
+  const addJob = async (newJob) => {
+    const res = await fetch(`/api/jobs`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // delete job
+  const deleteJobFunction = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+  // update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<SingleJobPage deleteJob={deleteJobFunction} />}
+          loader={jobLoader}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
 export default App;
+
+//components
+//props
+//statw
+//react router
+// dataLoader
+// useState
+// useParams
--- src/pages/jobPage.jsx
+++ src/pages/jobPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+// import React from "react";
+// import { useParams } from "react-router-dom";
 
-const jobPage = () => {
-  return <div>jobPage</div>;
-};
+// const JobPageNew = () => {
+//   const { id } = useParams();
+//   // const [job, setJob] = useState(null);
+//   // const [loading, setLoading] = useState(true);
 
-export default jobPage;
+//   //   useEffect(() => {
+//   //     const fetchJob = async () => {
+//   //       try {
+//   //         const res = await fetch(`/api/jobs/${id}`);
+//   //         const data = await res.json();
+//   //         setJob(data);
+//   //       } catch (error) {
+//   //         console.log("Error fetching data ", error);
+//   //       } finally {
+//   //         setLoading(false);
+//   //       }
+//   //     };
+//   //     fetchJob();
+//   //   }, []);
+//   //   return loading ? <Spinner /> : <h1>{job.title}</h1>;
+//   // };
+
+//   const jobLoader = async ({ params }) => {
+//     const res = await fetch(`/api.jobs/${params.id}`);
+//     const data = await res.json();
+//     return data;
+//   };
+// };
+// export { JobPageNew as default, jobLoader };
